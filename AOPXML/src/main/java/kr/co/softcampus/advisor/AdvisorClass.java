afaf4baa460a27eb74546cad745164622db7aba8
kho903 <gmldnr2222@naver.com>
2021-01-24 21:25:40 +0900

    AOP

---
+++ AOPXML/src/main/java/kr/co/softcampus/advisor/AdvisorClass.java
@@ -0,0 +1,36 @@
+package kr.co.softcampus.advisor;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AdvisorClass {
+
+	public void beforeMethod() {
+		System.out.println("beforeMethod 호출");
+	}
+
+	public void afterMethod() {
+		System.out.println("afterMethod 호출");
+	}
+
+	public Object aroundMethod(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("aroundMethod 호출1");
+		
+		// 원래의 메서드를 호출한다.
+		Object obj = pjp.proceed();
+		
+		System.out.println("aroundMethod 호출2");
+		
+		return obj;
+	}
+	
+	// 정상적으로 끝났을 때 리턴
+	public void afterReturningMethod() {
+		System.out.println("afterReturningMethod 호출");
+	}
+	
+	// 예외 발생시 리턴
+	public void afterThrowingMethod(Throwable e1) {
+		System.out.println("afterThrowingMethod 호출");
+		System.out.println(e1);
+	}
+}
---
+++ AOPXML/src/main/java/kr/co/softcampus/beans/TestBean.java
@@ -0,0 +1,10 @@
+package kr.co.softcampus.beans;
+
+public class TestBean {
+	public int method1() {
+		System.out.println("method1 호출");
+		// 예외 발생 코드
+//		int a = 1 / 0;
+		return 100;
+	}
+}
